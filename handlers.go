2a4ad2f149714fa00d24e85985e71f723ffccfd7
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-10-06 15:58:07 +0100

    added secure cookie implementation and automated cookie removal if it has expired on server side

--- handlers.go
+++ handlers.go
@@ -5,10 +5,42 @@ import (
 	"fmt"
 	"html/template"
 	"github.com/markbates/goth/gothic"
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/securecookie"
 )
 
+var hashGenKeySecret = securecookie.GenerateRandomKey(16)
+var blockGenKey = securecookie.GenerateRandomKey(16)
+
+// setting secure cookie instance
+var hashKey = []byte(hashGenKeySecret)
+var blockKey = []byte(blockGenKey)
+var s = securecookie.New(hashKey, blockKey)
+
+
 func homeHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Welcome to the home page!")
+
+	if cookie, err := r.Cookie("twauth"); err == nil {
+		log.Info("Cokkie found, decoding...")
+		value := make(map[string]string)
+		err := s.Decode("twauth", cookie.Value, &value)
+		if err == nil {
+			fmt.Fprintf(w, "The value of twtoken is %q", value["twtoken"])
+			fmt.Fprintf(w, "The value of twtokensecret is %q", value["twtokensecret"])
+		} else {
+			log.Error("failed to decode cookie", err)
+			// not valid, probably wrong key
+			// removing cookie and redirecting to login page
+			http.SetCookie(w, &http.Cookie{
+				Name: "twauth",
+				Value: "",
+				Path: "/",
+				MaxAge: -1,
+			})
+			w.Header()["Location"] = []string{"/login"}
+			w.WriteHeader(http.StatusTemporaryRedirect)
+		}
+	}
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request){
@@ -24,12 +56,25 @@ func callBackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// setting twauth cookie for later usage
-	http.SetCookie(w, &http.Cookie{
-		Name:  "twauth",
-		Value: user.AccessToken,
-		Path:  "/"})
-
+	// setting cookie value
+	value := map[string]string{
+		"twtoken": user.AccessToken,
+		"twtokensecret": user.AccessTokenSecret,
+	}
+	log.Info("got details, encoding cookie")
+	// encoding and setting cookie
+	encoded, err := s.Encode("twauth", value)
+	if err == nil {
+		cookie := &http.Cookie{
+			Name:  "twauth",
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(w, cookie)
+		log.Info("Cookie set")
+	} else {
+		log.Error("Failed to write cookie", err)
+	}
 
 	t, _ := template.New("foo").Parse(userTemplate)
 	t.Execute(w, user)
