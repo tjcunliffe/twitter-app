15ec68fb07fedbd062cb26a37cef45a57aed4645
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-10-07 09:37:33 +0100

    added provider wrapper

--- auth.go
+++ auth.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"net/http"
+	"github.com/go-zoo/bone"
 )
 
+// WithAuth function checks whether cookie exists at all. It doesn't check whether it is valid or not
+// valid or not is decided by actual handler that tries to decode it.
 func WithAuth(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
@@ -18,6 +21,19 @@ func WithAuth(fn http.HandlerFunc) http.HandlerFunc {
 			// success, proceed to original Handler
 			fn(w, r)
 		}
-//		fn(w, r)
+	}
+}
+
+
+// GetProvider function adds additional parameter provider to URL query since Gothic expects it there
+// https://github.com/markbates/goth/blob/master/gothic/gothic.go#L148-L160, this is just a function wrapper.
+// if switched to other mux - there wouldn't be a need for this
+func GetProvider(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		provider := bone.GetValue(r, "provider")
+		values := r.URL.Query()
+		values.Add("provider", provider)
+		r.URL.RawQuery = values.Encode()
+		fn(w, r)
 	}
 }
