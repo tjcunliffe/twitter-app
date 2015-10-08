7e77df54701eb08af2359a3ea15503c0ad0f6102
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-10-08 11:00:19 +0100

    properly formatting error message

--- handlers.go
+++ handlers.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	log "github.com/Sirupsen/logrus"
 	"github.com/unrolled/render"
+	"encoding/json"
 )
 
 // SearchQuery is a struct of json body that is expected to
@@ -16,6 +17,9 @@ type SearchQuery struct {
 	Secret string `json:secret`
 }
 
+type ErrorResponse struct {
+	Error    string `json:error`
+}
 
 type Handler struct {
 	r *render.Render
@@ -29,13 +33,22 @@ func (h *HTTPClientHandler) queryTwitter(w http.ResponseWriter, r *http.Request)
 
 	// getting query
 	urlQuery := r.URL.Query()
-	// getting session name
+	// getting submitted query string
 	queryString := urlQuery["q"]
+	// getting actual twitter (or Mirage) backend URI
+	backendUri := urlQuery["backend"]
 
 	client := h.http.HTTPClient
 	b := h.getBearerToken()
 
-	twitterEndPoint := TwitterUri + "/1.1/search/tweets.json?q=" + queryString[0]
+	twitterEndPoint := backendUri[0] + "/1.1/search/tweets.json?q=" + queryString[0]
+	// logging full URL and path
+	log.WithFields(log.Fields{
+		"chosenBackend": backendUri[0],
+		"query": queryString[0],
+		"finalTwitterEndpoint": twitterEndPoint,
+	}).Info("Endpoint created, performing query...")
+
 	req, err := http.NewRequest("GET", twitterEndPoint, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -49,16 +62,36 @@ func (h *HTTPClientHandler) queryTwitter(w http.ResponseWriter, r *http.Request)
 	//Issue the request and get the JSON API response
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err, resp)
-	}
-	defer resp.Body.Close()
-	// reading response body and returning it directly to the initial client, skipping decode step
-	respBody, err := ioutil.ReadAll(resp.Body)
+		// logging error
+		log.WithFields(log.Fields{
+			"Error": err.Error(),
+			"finalTwitterEndpoint": twitterEndPoint,
+		}).Warn("Got error while querying external API...")
 
-	if err != nil {
-		log.Fatal(err)
+		// creating JSON response with error
+		errString := err.Error()
+		errorMsg := ErrorResponse{errString}
+		js, err := json.Marshal(errorMsg)
+
+		if err != nil {
+			log.Error("Got error while marshalling error..")
+			return
+		}
+        // writing response js
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+
+	} else {
+		defer resp.Body.Close()
+		// reading response body and returning it directly to the initial client, skipping decode step
+		respBody, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(respBody)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(respBody)
 }
