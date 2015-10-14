06f19b63eb4b9352b233873ce3b41d8ca08ec765
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-10-14 10:38:01 +0100

    added tooltip for external api selection, cleanup

--- static/scripts/twsearch.jsx
+++ static/scripts/twsearch.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ProgressBar, Input, ButtonInput } from 'react-bootstrap'
+import { ProgressBar, Input, ButtonInput, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import Highlight from 'react-highlight'
 var pd = require('pretty-data').pd;
 
@@ -24,8 +24,6 @@ const TweetsResultsComponent = React.createClass({
             dataType: "json",
             url: this.state.url,
             success: function (data) {
-                console.log(data);
-
                 if (that.isMounted()) {
                     that.setState({
                         data: data,
@@ -56,13 +54,13 @@ const TweetsResultsComponent = React.createClass({
             );
             return <div> {preInstance} </div>
         }
-
-
     }
 });
 
 
 const ButtonInputQueryComponent = React.createClass({
+    displayName: "ButtonInputQueryComponent",
+
     getInitialState() {
         return {
             disabled: true,
@@ -79,10 +77,14 @@ const ButtonInputQueryComponent = React.createClass({
 
         this.state.ss = backend == "https://api.twitter.com";
 
-        if(backend != "http://localhost:8300"){
-            if (length > 0) style = 'success';
+        if (backend != "http://localhost:8300") {
+            if (length > 0) {
+                style = 'success'
+            }
         } else {
-            if (length >0 && scenarioSession > 0) style = 'success';
+            if (length > 0 && scenarioSession > 0) {
+                style = 'success'
+            }
         }
 
         //else if (length > 5) style = 'warning';
@@ -92,11 +94,13 @@ const ButtonInputQueryComponent = React.createClass({
         return {style, disabled};
     },
 
-    handleChange() {
+    handleChange()
+    {
         this.setState(this.validationState());
     },
 
-    handleSubmit(e) {
+    handleSubmit(e)
+    {
 
         e.preventDefault();
         let query = this.refs.input.getValue();
@@ -106,9 +110,6 @@ const ButtonInputQueryComponent = React.createClass({
 
         // creating final href
         var href = "/query?q=" + query + "&backend=" + backend + "&ss=" + scenarioSession;
-        //var href = "/rand?q=" + query;
-        console.log("preparing query");
-        console.log(href);
 
         // unmounting current results
         React.unmountComponentAtNode(document.getElementById('results'));
@@ -116,21 +117,30 @@ const ButtonInputQueryComponent = React.createClass({
         React.render(<TweetsResultsComponent url={href}/>, document.getElementById("results"))
     },
 
-    render() {
+    render()
+    {
+
+        const APIDropdownTooltip = (
+            <Tooltip>Select external API URI to use. Choosing "Twitter" will call Twitter directly.
+                Choose "Mirage Proxy" for recording and playback functionality</Tooltip>
+        );
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-
-                    <Input type="select" label="External API URI" placeholder="select" ref="backend" onChange={this.handleChange}>
-                        <option value="https://api.twitter.com">Twitter</option>
-                        <option value="http://localhost:8300">Mirage Proxy</option>
-                    </Input>
+                    <OverlayTrigger placement='right' overlay={APIDropdownTooltip}>
+                        <Input type="select" label="External API URI" placeholder="select" ref="backend"
+                               onChange={this.handleChange}>
+                            <option value="https://api.twitter.com">Twitter</option>
+                            <option value="http://localhost:8300">Mirage Proxy</option>
+                        </Input>
+                    </OverlayTrigger>
 
                     <Input type="text" ref="scenarioSession" placeholder="Provide 'scenario:session' here"
                            disabled={this.state.ss}
                            onChange={this.handleChange}/>
 
-                    <Input type="text" ref="input" placeholder="Enter your query here.." onChange={this.handleChange}/>
+                    <Input type="text" ref="input" placeholder="Enter your query here.."
+                           onChange={this.handleChange}/>
 
                     <ButtonInput type="submit" value="Submit"
                                  bsStyle={this.state.style} bsSize="small"
